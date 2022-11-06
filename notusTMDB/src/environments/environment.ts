bb7d1a4bffb1c1ec28e97052e150ab305fb19ce9
Ana Pilar <pilarbarba03@gmail.com>
2022-11-07 00:47:56 +0100

    Terminado el listado de las series junto con sus fotos pero sin ningún estilo aplicado aún, esto se le añadirá cuando comience con los detalles

--- notusTMDB/src/environments/environment.ts
+++ notusTMDB/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_BASE_URL: 'https://developers.themoviedb.org/3',
+  API_BASE_URL: 'https://api.themoviedb.org/3',
   API_KEY: 'e375f35a8ed2c4c685f14c49cc598088',
 };
 
